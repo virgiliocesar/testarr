d726fcfe7c015c4e83279a94ce50ae68381f7adf
virgílio conceição césar <virgilio_cesar.dev@outlook.com>
2024-09-27 08:51:16 -0300

    ajust

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,10 +15,6 @@ conn();
 //? Routes
 app.use("/api/animals", animalsRoutes);
 
-app.get("/", (req, res) => {
-  res.redirect("/api/animals");
-});
-
 app.listen(process.env.PORT, () => {
   console.log("Servidor online");
 });
